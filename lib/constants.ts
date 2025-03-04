1f112d469b75905c1eceb509a4ff8441c258e258
ahmdhzq <haziexahmad18@gmail.com>
2025-03-05 02:05:51 +0700

    feat: product details page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Shoplines'
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more'
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Shoplines is a modern e-commerce platform'
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
