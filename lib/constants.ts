2900e702e96da0600f8675278662207de573a34e
ahmdhzq <haziexahmad18@gmail.com>
2025-03-05 02:34:28 +0700

    Feat: Implement Add To Cart

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,3 +2,6 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Shoplines'
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more'
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Shoplines is a modern e-commerce platform'
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
+export const FREE_SHIPPING_MIN_PRICE = Number(
+    process.env.FREE_SHIPPING_MIN_PRICE || 35
+)
